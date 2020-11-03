c5d47ed25465e48543c59cced6b324d0eb32d177
Onore849 <rion3376@gmail.com>
2020-11-03 18:59:43 +0900

    Initial Commit

---
+++ reviewInsta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reviewInsta
+//
+//  Created by 野澤拓己 on 2020/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
